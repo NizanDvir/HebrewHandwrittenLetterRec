3d0d500e569e167645cfb4b41799e93eb631c460
NizanDvir <nizandvir2005@gmail.com>
2024-08-01 19:55:33 +0300

    inital files

---
+++ server/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify
+import io
+import base64
+from PIL import Image, ImageCms
+import models.exported_methods as rec_model
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+
+# load the svm model
+rec_model.load_svm_model()
+
+
+@app.get('/options')
+def options():
+    return {
+        'width': 40,
+        'height': 40,
+        'pixelsPerCell': 12,
+    }
+
+
+@app.post('/save')
+@cross_origin()
+def save():
+    print('Saving image')
+    data: object = request.json
+    data_url_string: str = data['dataURL'].split(',')[1]
+    data_url_bytes = io.BytesIO(
+        base64.decodebytes(bytes(data_url_string, "utf-8")))
+    img = Image.open(data_url_bytes)
+
+    # get pixel values
+    width, height = img.size
+
+    # every 12 pixels is a cell
+    pixels_per_cell = options()['pixelsPerCell']
+
+    # create new image
+    new_img = Image.new('RGBA', (width // pixels_per_cell,
+                        height // pixels_per_cell), (255, 0, 0, 0))
+    new_img_pixels = new_img.load()
+    for x in range(0, width, pixels_per_cell):
+        for y in range(0, height, pixels_per_cell):
+            r, g, b, a = img.getpixel((x, y))
+            # if alpha is 0 dont draw
+            if a == 0:
+                # paint white
+                r, g, b = 255, 255, 255
+                new_img_pixels[x // pixels_per_cell,
+                               y // pixels_per_cell] = (r, g, b)
+            new_img_pixels[x // pixels_per_cell,
+                           y // pixels_per_cell] = (r, g, b)
+
+    alpha_values = [0 if r < 200 else 1 for r,
+                    _, _, _ in new_img.getdata()]
+
+    new_pixels = [(pixel * 255, pixel * 255, pixel * 255)
+                  for pixel in alpha_values]
+
+    # create new image
+    new_img = Image.new('RGB', (width // pixels_per_cell,
+                        height // pixels_per_cell), (255, 255, 255))
+    new_img.putdata(new_pixels)
+
+    # save the image
+    new_img.save('image.png')
+
+    labels = rec_model.predict_image(img)
+
+    response = {
+        'labels': labels,
+        'message': 'Image saved'
+    }
+
+    return jsonify(kwargs=response, status=200, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ server/models/exported_methods.py
@@ -0,0 +1,62 @@
+from PIL import Image
+import pickle
+import numpy as np
+import keras
+from sklearn import svm
+import cv2
+
+global rec_model
+
+
+def load_model():
+    global rec_model
+    # load the svm model
+    rec_model = keras.models.load_model('models/keras_model.keras')
+
+
+def convert_to_probability_dictionary(probabilities):
+    # convert the probabilities to a dictionary
+    probability_dict = {}
+    for i in range(len(probabilities)):
+        probability_dict[i] = float(probabilities[i])
+    return probability_dict
+
+
+def convert_img_to_binary_array(pixels):
+    # get the pixel values of the image
+    binary_array = []
+    for pixel in pixels:
+        if pixel[0] >= 128 and pixel[1] >= 128 and pixel[2] >= 128:
+            binary_array.append(1)
+        else:
+            binary_array.append(0)
+    return binary_array
+
+
+def convert_image(image):
+    print(image.shape)
+    # Resize the image to 40x40 if it's not already
+    if image.shape[:2] != (40, 40):
+        image = cv2.resize(image, (40, 40), interpolation=cv2.INTER_AREA)
+
+    # Convert to grayscale if the image has more than one channel
+    if len(image.shape) > 2:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Convert to black and white (0 or 255)
+    _, bw_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+
+    # Normalize to 0 or 1 for the neural network
+    normalized_image = (bw_image / 255).astype(np.float32)
+
+    # Reshape to add channel dimension
+    reshaped_image = normalized_image.reshape(1, 40, 40, 1)
+
+    return reshaped_image
+
+
+def predict_image(img):
+    img = convert_image(np.array(img))
+    predicted = rec_model.predict([img])
+    print(convert_to_probability_dictionary(predicted[0]))
+    return convert_to_probability_dictionary(predicted[0])
